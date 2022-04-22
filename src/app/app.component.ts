2159c4f863c4e5d5cee3b9c3ff12229f44ce5611
heshanu <heshanu97@gmail.com>
2022-04-22 10:14:16 -0700

    Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AngularStyleandGraphics';
-  isFavourite=false;
-  showBoring=false;
-  onShowBoring(element:HTMLElement){
-          element.style.display='block';
-  }
-  courseGoals = [
-    { title: 'Master Angular Styling', isActiveGoal: true },
-    { title: 'Understand Angular Animations', isActiveGoal: false },
-    { title: 'Master Angular Animations', isActiveGoal: false },
-  ];
+  isFavourite = false;
+  showBoring = false;
 
+  constructor(private render: Renderer2) {}
+
+  onShowBoring(element: HTMLElement) {
+    this.render.setStyle(element, 'display', 'block');
+  }
 }
